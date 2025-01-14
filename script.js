62bccedf2e369d1bea9988e187ae2fde39a69420
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-01-14 09:22:30 +0700

    Add constants & variable into script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Constants Declaration
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const container             = document.querySelector("#container");
+const horizontalContainer   = document.querySelector(".horizontal-container");
+const firstSquare           = document.createElement("div");
+firstSquare.classList.add("square");
+const squareSelector        = ".square";
+const hoverEvent            = "mouseover";
+
+
+// Variable Declaration
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let gridSize = 16;
+
+
+// Functions Declaration
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
