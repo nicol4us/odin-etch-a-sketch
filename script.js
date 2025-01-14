0c7abd0787ad6fc33fc6fde408c92fcbd0da16a4
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-01-14 10:24:49 +0700

    Add createGrid function into script.js

--- script.js
+++ script.js
@@ -13,5 +13,26 @@ const hoverEvent            = "mouseover";
 let gridSize = 16;
 
 
+
 // Functions Declaration
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//(Element, Element, Integer) -> ()
+// To duplicate and and append element into parent element
+function duplicateAndAppendElement(parentElement, childElement, number) {
+    for(i=0; i < number; i++) {
+        parentElement.appendChild(childElement.cloneNode(true));
+    }
+}
+
+//(Element, Element, Element, Integer) -> ()
+// To create grid according to number
+function createGrid(mainContainer, horizontalContainer, element, number) {
+    duplicateAndAppendElement(horizontalContainer,element,number);
+    duplicateAndAppendElement(mainContainer,horizontalContainer, (number-1));
+}
+
+
+
+
+
